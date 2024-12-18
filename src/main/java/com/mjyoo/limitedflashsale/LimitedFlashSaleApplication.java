47d0a94f24e1f9840dcaae998464c97baaf37ebf
mjyoo <ryuminji0353@gmail.com>
2024-12-18 00:08:03 -0800

    initial commit - Add initial project files

---
+++ src/main/java/com/mjyoo/limitedflashsale/LimitedFlashSaleApplication.java
@@ -0,0 +1,13 @@
+package com.mjyoo.limitedflashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedFlashSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedFlashSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjyoo/limitedflashsale/LimitedFlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjyoo.limitedflashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedFlashSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
