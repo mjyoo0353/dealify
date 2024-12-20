33890ec0b2b93a42beb5159c48d383bcdf1261f4
mjyoo <ryuminji0353@gmail.com>
2024-12-20 01:40:11 -0800

    [feat] add JwtUtil for JWT creation, validation, and information retrieval

---
+++ src/main/java/com/mjyoo/limitedflashsale/jwt/JwtUtil.java
@@ -0,0 +1,103 @@
+package com.mjyoo.limitedflashsale.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtUtil {
+    //Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    //Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+    //Token 만료 시간
+    private final long TOKEN_EXPIRATION_MS = 60 * 60 * 1000L; // 1시간
+
+    @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    //로그 설정
+    public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    //토큰 생성
+    public String createToken(String username) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username) //사용자 식별자값 (ID)
+                        .setExpiration(new Date(date.getTime() + TOKEN_EXPIRATION_MS)) //생성되는 시간 기준으로 만료 시간 계산
+                        .setIssuedAt(date) //토큰 발급일
+                        .signWith(key, signatureAlgorithm) //암호화 알고리즘
+                        .compact();
+    }
+
+    //JWT Cookie에 저장
+    public void addJwtToCookie(String token, HttpServletResponse response) {
+        try {
+            // 토큰에 공백이 있을 경우 인코딩
+            token = URLEncoder.encode(token, "UTF-8").replaceAll("\\+", "%20");
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token);
+            cookie.setPath("/");
+            //Response에 Cookie 추가
+            response.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    //Cookie에 들어있던 JWT 토큰을 Substring
+    public String substringToken(String tokenValue) {
+        if(StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(BEARER_PREFIX.length());
+        }
+        logger.error("Not Found Token");
+        throw new NullPointerException("JWT Token not found in request header or cookie");
+    }
+
+    //토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("invalid JWT signature: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    //토큰에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+}
