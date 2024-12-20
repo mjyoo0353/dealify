2a618b00724f63c646ea07d1da9004030b4afd9c
mjyoo <ryuminji0353@gmail.com>
2024-12-20 08:11:43 -0800

    [feat] Add Timestamped class to manage created_at and modified_at fields

--- src/main/java/com/mjyoo/limitedflashsale/LimitedFlashSaleApplication.java
+++ src/main/java/com/mjyoo/limitedflashsale/LimitedFlashSaleApplication.java
@@ -2,9 +2,14 @@ package com.mjyoo.limitedflashsale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class LimitedFlashSaleApplication {
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Disable Spring Security
 
     public static void main(String[] args) {
         SpringApplication.run(LimitedFlashSaleApplication.class, args);
