458b62fb81bda49c41fa66ab820ec7594f7d5927
mjyoo <ryuminji0353@gmail.com>
2024-12-30 22:12:07 -0800

    [feat] Implement FlashSale opening and closing features
    - Add time validation for opening FlashSale events

--- src/main/java/com/mjyoo/limitedflashsale/event/controller/FlashSaleController.java
+++ src/main/java/com/mjyoo/limitedflashsale/event/controller/FlashSaleController.java
@@ -1,37 +1,69 @@
 package com.mjyoo.limitedflashsale.event.controller;
 
+import com.mjyoo.limitedflashsale.auth.security.UserDetailsImpl;
 import com.mjyoo.limitedflashsale.common.dto.ApiResponse;
 import com.mjyoo.limitedflashsale.event.dto.FlashSaleRequestDto;
 import com.mjyoo.limitedflashsale.event.service.FlashSaleService;
+import com.mjyoo.limitedflashsale.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/event")
+@RequestMapping("/api/flash-sale")
 public class FlashSaleController {
 
     private final FlashSaleService flashSaleService;
 
-    // TODO 행사 조회
+    // 행사 조회
+    @GetMapping("/{eventId}")
+    public ResponseEntity<ApiResponse<?>> getFlashSale(@PathVariable Long eventId) {
+        return ResponseEntity.ok(ApiResponse.success("행사 조회 성공"));
+    }
 
-    // TODO 행사 목록 조회
+    // 행사 목록 조회
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse<?>> getFlashSaleList() {
+        return ResponseEntity.ok(ApiResponse.success("행사 목록 조회 성공"));
+    }
 
     // 행사 생성
     @PostMapping("/admin/create")
-    public ResponseEntity<ApiResponse<?>> createEvent(@Valid @RequestBody FlashSaleRequestDto requestDto) {
-        Long eventId = flashSaleService.createEvent(requestDto);
+    public ResponseEntity<ApiResponse<?>> createFlashSale(@Valid @RequestBody FlashSaleRequestDto requestDto,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        Long eventId = flashSaleService.createFlashSale(requestDto, user);
         return ResponseEntity.ok(ApiResponse.success("행사가 생성되었습니다.", eventId));
     }
 
-    // TODO 행사 수정
+    // 행사 수정
+    @PutMapping("/admin/update/{eventId}")
+    public ResponseEntity<ApiResponse<?>> updateFlashSale(@PathVariable Long eventId,
+                                                      @Valid @RequestBody FlashSaleRequestDto requestDto,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        flashSaleService.updateFlashSale(eventId, requestDto, user);
+        return ResponseEntity.ok(ApiResponse.success("행사가 수정되었습니다."));
+    }
+
+    // 행사 오픈
+    @PutMapping("/admin/open/{eventId}")
+    public ResponseEntity<ApiResponse<?>> openFlashSale(@PathVariable Long eventId,
+                                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        flashSaleService.openFlashSale(eventId, user);
+        return ResponseEntity.ok(ApiResponse.success("행사가 오픈되었습니다."));
+    }
 
     // 행사 종료
-    @PutMapping("/admin/end/{eventId}")
-    public ResponseEntity<ApiResponse<?>> deleteEvent(@PathVariable Long eventId) {
-        flashSaleService.deleteEvent(eventId);
+    @PutMapping("/admin/close/{eventId}")
+    public ResponseEntity<ApiResponse<?>> deleteFlashSale(@PathVariable Long eventId,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        flashSaleService.closeFlashSale(eventId, user);
         return ResponseEntity.ok(ApiResponse.success("행사가 종료되었습니다."));
     }
 }
--- src/main/java/com/mjyoo/limitedflashsale/event/service/FlashSaleService.java
+++ src/main/java/com/mjyoo/limitedflashsale/event/service/FlashSaleService.java
@@ -12,12 +12,15 @@ import com.mjyoo.limitedflashsale.event.repository.FlashSaleProductRepository;
 import com.mjyoo.limitedflashsale.event.repository.FlashSaleRepository;
 import com.mjyoo.limitedflashsale.product.entity.Product;
 import com.mjyoo.limitedflashsale.product.repository.ProductRepository;
+import com.mjyoo.limitedflashsale.user.entity.User;
+import com.mjyoo.limitedflashsale.user.entity.UserRoleEnum;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -27,12 +30,17 @@ public class FlashSaleService {
     private final FlashSaleRepository flashSaleRepository;
     private final FlashSaleProductRepository flashSaleProductRepository;
 
+    //TODO 행사 조회
+    //TODO 행사 목록 조회
+
     //행사 생성
     @Transactional
-    public Long createEvent(@Valid FlashSaleRequestDto requestDto) {
+    public Long createFlashSale(@Valid FlashSaleRequestDto requestDto, User user) {
+        // 관리자 권한 확인
+        checkAdminRole(user);
+
         // 상품 조회
-        Product product = productRepository.findById(requestDto.getProductId())
-                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+        Product product = getProduct(requestDto);
 
         // 행사 생성 및 저장
         FlashSale flashSale = FlashSale.builder()
@@ -61,9 +69,56 @@ public class FlashSaleService {
         return flashSale.getId();
     }
 
-    //TODO 행사 삭제 - Soft Delete
-    public void deleteEvent(Long eventId) {
+    // TODO 행사 수정
+    public void updateFlashSale(Long eventId, FlashSaleRequestDto requestDto, User user) {
+    }
 
+    //행사 시작
+    @Transactional
+    public void openFlashSale(Long eventId, User user) {
+        // 관리자 권한 확인
+        checkAdminRole(user);
+
+        // 행사 상품 조회
+        FlashSaleProduct flashSaleProduct = getFlashSaleProduct(eventId);
+
+        // 행사 조회
+        FlashSale flashSale = flashSaleProduct.getFlashSale();
+        // 행사 시작 시간이 현재 시간보다 미래인지 확인하고 미래라면 예외 발생
+        if(LocalDateTime.now().isBefore(flashSale.getStartTime())) {
+            throw new CustomException(ErrorCode.FLASH_SALE_NOT_STARTED);
+        }
+        flashSaleProduct.getFlashSale().setStatus(FlashSaleStatus.ACTIVE);
+        flashSaleProductRepository.save(flashSaleProduct);
+    }
+
+    //행사 종료
+    @Transactional
+    public void closeFlashSale(Long eventId, User user) {
+        // 관리자 권한 확인
+        checkAdminRole(user);
+
+        // 행사 상품 조회
+        FlashSaleProduct flashSaleProduct = getFlashSaleProduct(eventId);
+
+        flashSaleProduct.getFlashSale().setStatus(FlashSaleStatus.ENDED);
+        flashSaleProductRepository.save(flashSaleProduct);
+    }
+
+    private Product getProduct(FlashSaleRequestDto requestDto) {
+        return productRepository.findById(requestDto.getProductId())
+                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+    }
+
+    private FlashSaleProduct getFlashSaleProduct(Long eventId) {
+        return flashSaleProductRepository.findById(eventId)
+                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+    }
+
+    private void checkAdminRole(User user) {
+        if (user == null && !user.getRole().equals(UserRoleEnum.ADMIN)) {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
     }
 
 }
